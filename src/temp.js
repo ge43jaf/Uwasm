8c97915f393dbf52eb2af76778ea7e8a96d0a393
Jason <73897542+ge43jaf@users.noreply.github.com>
2025-05-26 11:16:12 +0200

    Update temp.js
    Partial set implementation of numeric instructions

--- src/temp.js
+++ src/temp.js
@@ -7,8 +7,8 @@ const importObject = {
 // Assume `wasmModule` is already compiled using WebAssembly.Module
 const wasmInstance = new WebAssembly.Instance(wasmModule, importObject);
 
-const { init_log } = wasmInstance.exports;
+const { init } = wasmInstance.exports;
 
 for (let i = 0; i < 10; i++) {
-  init_log();
+  init();
 }
