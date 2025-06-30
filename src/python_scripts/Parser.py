4c3e78c19173c12a0437a3193dc1b1be068566e8
Jason <73897542+ge43jaf@users.noreply.github.com>
2025-06-30 11:58:49 +0200

    Update Parser.py
    Support parsing the module and first function

--- src/python_scripts/Parser.py
+++ src/python_scripts/Parser.py
@@ -40,34 +40,76 @@ class Parser:
 
 
     def parse(self, tokens):
+        return self.parse_module(tokens)
         
+    def parse_module(self, tokens):    
         if not tokens:
+            print("CompilationError: Token list ot initialized")
             return None
-        
+            
+        if len(tokens) == 0:
+            print("SyntaxError: Expected ( at the start of the program")
+            return None
+            
         # Should first check (module ...)
         token = tokens.pop(0)
+        print('Token : "' + token + '" ')
         if token != '(':
             print("SyntaxError: Expected ( at the start of the program")
             return None
         
         token = tokens.pop(0)
+        print('Token : "' + token + '" ')
+        
         if token != 'module':
             print("SyntaxError: Expected a module at the beginning")
             return None
         
-        token = tokens.pop(0)
-        if token == '(':
-            expression = []
-            while tokens and tokens[0] != ')':
-                expression.append(self.parse(tokens))
-            if tokens:
-                tokens.pop(0)
-            return expression
-        elif token == ')':
-            print("SyntaxError: Unexpected )")
+        if tokens[-1] != ')':
+            print("SyntaxError: Expected ) at the end of the program")
             return None
+        
+        token = tokens.pop(0)
+        print('Token : "' + token + '" ')
+        
+        if token == ')':
+            print("Syntax Correct!)")       # A little duplicate here
+            return None
+        elif token == '(':
+            # expression = []
+            # while tokens and tokens[0] != ')':
+            #     expression.append(self.parse(tokens))
+            # if tokens:
+            #     tokens.pop(0)
+            # return expression
+            
+            return self.parse_functions(token, tokens)
+            
         else:
-            return self.validate(token, tokens)
+            # return self.validate(token, tokens)
+            print("SyntaxError: All terms7expression surrounded with (...) expected")
+            return None
+        
+    def parse_functions(self, token, tokens):
+        
+        funcs = {}
+        
+        token = tokens.pop(0)
+        print('parse_functions Token : "' + token + '" ')
+        
+        if token != 'func':
+            print("SyntaxError: Only func supported after module")
+            return None
+            
+        token = tokens.pop(0)
+        print('parse_functions Token : "' + token + '" ')
+        
+        if token[0] == '$':         # TODO: Further validations waited
+            funcs.add(token[1:])
+        elif token == '(':
+            
+            
+            
         
     def validate(self, instruction, tokens):
         
